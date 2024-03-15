38517b725ff8d3cc1407a79c1ed3be938ea41df9
AlexKohanik <118318185+AlexKohanik@users.noreply.github.com>
2024-03-14 20:27:19 -0400

    Update Admin_exchangeOnlineHiddenRules.ps1

--- Scripts/Admin/Admin_exchangeOnlineHiddenRules.ps1
+++ Scripts/Admin/Admin_exchangeOnlineHiddenRules.ps1
@@ -1,8 +1,13 @@
 # Check for Hidden Inbox rules using exchange online module. 
+# Install-Module -Name ExchangeOnlineManagement -RequiredVersion 3.4.0    <--- Install if necessary
 
-Connect-ExchangeOnline -UserPrincipalName Read-Host "Enter UPN" -ShowProgress $true
+Import-Module ExchangeOnlineManagement
+
+$UPN = Read-Host "Enter UPN"
+
+Connect-ExchangeOnline -UserPrincipalName $UPN -ShowProgress $true
 
 Get-InboxRule -Mailbox Read-Host "Enter employee email address" | Where-Object {$_.IsEnabled -eq $true -and $_.Visible -eq $false}
 
-# Run this after to disconnect from online module. 
+# Run this after. 
 #Disconnect-ExchangeOnline -Confirm:$false
